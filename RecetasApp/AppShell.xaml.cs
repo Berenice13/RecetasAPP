f299a67a717c5123e44af1504837e7f57faad3a3
Berenice de la Cruz de la Cruz <75514306+Berenice13@users.noreply.github.com>
2024-11-04 09:41:16 -0600

    Initial commit

---
+++ RecetasApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RecetasApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RecetasApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RecetasApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
